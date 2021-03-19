09b25a2e785896e2f0b6ff22eb207163a1cddb6b
Debasish Gracias <debzu57@gmail.com>
2021-03-19 21:01:48 +0000

    added framer motion and project images

---
+++ components/WebProject.js
@@ -0,0 +1,47 @@
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from 'react'
+function WebProject({ title, description, techstack, desktopimage, mobileimage, livelink, codelink ,isBorderRemove}) {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
+    const imageVariants = {
+        visible: { opacity: 1, scale: 1, transition: { duration: 0.6 } },
+        hidden: { opacity: 0, scale: 0 }
+    }
+    return (
+        <div className="project">
+            <div className="project-images">
+                <motion.img
+                    ref={ref}
+                    animate={controls}
+                    initial="hidden"
+                    variants={imageVariants}
+                    alt="tmdb screenshot" loading="lazy" className="big" src={desktopimage} />
+                <motion.img
+                    ref={ref}
+                    animate={controls}
+                    initial="hidden"
+                    transition={{ duration: 0.3 }}
+                    variants={imageVariants}
+                    alt="tmdb mobile screenshot" loading="lazy" className={"small " + (isBorderRemove ? 'remove-small' : null)}  src={mobileimage} />
+            </div>
+            <div className="project-details">
+                <h4>{title}</h4>
+                <p className="project-desc">{description}</p>
+                <p className="stack"><b>Tech Stack: </b>{techstack}</p>
+                <div className="buttons">
+                    <a rel="noreferrer" target="_blank" href={livelink} className="button">Live Preview</a>
+                    <a rel="noreferrer" target="_blank" href={codelink} className="buttonText">View Code</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WebProject
--- components/Work.js
+++ components/Work.js
@@ -1,16 +1,9 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import WebProject from './WebProject';
+
 function Work() {
-    // const styleWeb = {
-    //     display: 'block',
-    //     transform: `translate(${0}px, ${0}px)`,
-    //     opacity: 1
-    // };
-    // const styleMobile = {
-    //     display: 'none',
-    //     transform: `translate(${0}px, ${50}px)`,
-    //     opacity: 0
-    // };
+
     const styleImage = {
         transform: `translate(${0}px, ${0}px)`
     };
@@ -53,51 +46,18 @@ function Work() {
                     </div>
                 </div>
                 <div className="projects" id="webprojects" style={isWebChecked ? { display: 'block' } : { display: 'none' }}>
-                    <div className="project">
-                        <div className="project-images">
-                            <img alt="amazon screenshot" loading="lazy" className="big" src="images/amazon.00b1ee4aae56d4ccd0607c7828fa1ab6.webp" style={styleImage} />
-                            <img alt="amazon mobile screenshot" loading="lazy" className="small" src="images/amazon-small.a648a4e261b1ec33c21f69c0f9063aa8.webp" style={styleImage} />
-                        </div>
-                        <div className="project-details">
-                            <h4>Netflix Clone</h4>
-                            <p className="project-desc">Netflix clone built using ReactJs and Redux with complete payment functionality(Stripe API) and user authentication (Firebase). Movies are fetched using the TMDb API.</p>
-                            <p className="stack"><b>Tech Stack: </b>React Router, Redux, Payment Processing with Stripe API, Firebase, Material UI</p>
-                            <div className="buttons">
-                                <a rel="noreferrer" target="_blank" href="https://netflix-clone-e9ce7.web.app" className="button">Live Preview</a>
-                                <a rel="noreferrer" target="_blank" href="https://github.com/Deba22/netflix-clone" className="buttonText">View Code</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="project">
-                        <div className="project-images">
-                            <img alt="tmdb screenshot" loading="lazy" className="big" src="images/tmdb.487ae9dd26c3f72c98e8338766eb7c69.webp" style={styleImage} />
-                            <img alt="tmdb mobile screenshot" loading="lazy" className="small" src="images/tmdb-small.f5969f9b77fdfc4d135bffa8210bccf7.webp" style={styleImage} />
-                        </div>
-                        <div className="project-details">
-                            <h4>Hello Chat App</h4>
-                            <p className="project-desc">A real time chat app powered by React and Firebase. The user has the ability to create private and/or group chat rooms. </p>
-                            <p className="stack"><b>Tech Stack: </b>ReactJs, Redux, Firebase, Google Authentication, Material UI</p>
-                            <div className="buttons">
-                                <a rel="noreferrer" target="_blank" href="https://chatapp-firebase-b3780.web.app" className="button">Live Preview</a>
-                                <a rel="noreferrer" target="_blank" href="https://github.com/Deba22/Hello-Chat-App" className="buttonText">View Code</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="project">
-                        <div className="project-images">
-                            <img alt="spotify screenshot" loading="lazy" className="big" src="images/spotify.a7c9f4381c6bb80475411bb5d00147ec.webp" style={styleImage} />
-                            <img alt="spotify mobile screenshot" loading="lazy" className="small" src="images/spotify-small.8af8c646fb53f6a3e0b954dba265800e.webp" style={styleImage} />
-                        </div>
-                        <div className="project-details">
-                            <h4>Snapchat Clone</h4>
-                            <p className="project-desc">Snapchat clone developed using React and Firebase which lets users exchange pictures that are meant to disappear after they're viewed.</p>
-                            <p className="stack"><b>Tech Stack: </b>ReactJs, Redux, Firebase, Google Authentication, Camera, Material UI</p>
-                            <div className="buttons">
-                                <a rel="noreferrer" target="_blank" href="https://snapchat-clone-ccadd.web.app" className="button">Live Preview</a>
-                                <a rel="noreferrer" target="_blank" href="https://github.com/Deba22/snapchat-clone" className="buttonText">View Code</a>
-                            </div>
-                        </div>
-                    </div>
+                    <WebProject title="Netflix Clone" description="Netflix clone built using ReactJs and Redux with complete payment functionality(Stripe API) and user authentication (Firebase). Movies are fetched using the TMDb API."
+                        techstack="React Router, Redux, Payment Processing with Stripe API, Firebase, Material UI" desktopimage="/netflix-desktop.png" mobileimage="/netflix-mobile.png"
+                        livelink="https://netflix-clone-e9ce7.web.app/" codelink="https://github.com/Deba22/netflix-clone"
+                    />
+                    <WebProject title="Hello Chat App" description="A real time chat app powered by React and Firebase. The user has the ability to create private and/or group chat rooms."
+                        techstack="ReactJs, Redux, Firebase, Google Authentication, Material UI" desktopimage="/chat-desktop.png" mobileimage="/chat-mobile.png"
+                        livelink="https://chatapp-firebase-b3780.web.app" codelink="https://github.com/Deba22/Hello-Chat-App"
+                    />
+                    <WebProject title="Snapchat Clone" description="Snapchat clone developed using React and Firebase which lets users exchange pictures that are meant to disappear after they're viewed."
+                        techstack="ReactJs, Redux, Firebase, Google Authentication, Camera, Material UI" desktopimage="/snapchat-desktop.png" mobileimage="/snapchat-mobile.png"
+                        livelink="https://snapchat-clone-ccadd.web.app" codelink="https://github.com/Deba22/snapchat-clone" isBorderRemove
+                    />
 
                 </div>
                 <div className="designs" id="mobileprojects" style={isMobileChecked ? { display: 'block' } : { display: 'none' }}>
