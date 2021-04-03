826945d2f273805d207078b53c8e96f14329d704
Debasish Gracias <debzu57@gmail.com>
2021-04-03 09:06:04 +0100

    GTM integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 import { ToastProvider, useToasts } from 'react-toast-notifications'
-
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 function MyApp({ Component, pageProps }) {
-  
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5N5VDRT' })
+  }, [])
   return (
     <ToastProvider
       placement="bottom-right">
