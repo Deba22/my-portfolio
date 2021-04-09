6133519ae7c85e8522300e2e8f52a51d3ab77d0d
Debasish Gracias <debzu57@gmail.com>
2021-04-09 16:05:35 +0100

    dummy change

--- components/Meta.js
+++ components/Meta.js
@@ -6,7 +6,7 @@ function Meta({ title, keywords, description }) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta property="og:locale" content="en_IN"></meta>
             <title>{title}</title>
-            <meta name='description' content={description} />
+            <meta name='description' content={description} /> 
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
             <meta property="”og:url”" content="https://debasishgracias.com" />
