3c4d1932ba63f04151746e5abed40aa0fce1b4ba
Debasish Gracias <debzu57@gmail.com>
2021-04-09 15:55:21 +0100

    environment variable changes

--- config/index.js
+++
@@ -1,4 +0,0 @@
-const dev = process.env.Node_ENV !== 'production'
-
-export const airtableApiKey = 'keyfk9N6xZALtPjLK'
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
