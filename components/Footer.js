3328d46cdc8011a69fdbaf12dca7dff8b34fc761
Debasish Gracias <d.gracias@clerkswell.com>
2024-08-16 21:05:59 +0530

    Aboutus and footer changes

--- components/Footer.js
+++ components/Footer.js
@@ -39,7 +39,7 @@ function Footer() {
                 <span className="copyright">
                     Goa, India <br></br>
         debagracias@gmail.com <br></br>
-        Copyright © 2023
+        Copyright © 2024
         </span>
             </div>
         </footer>
